8c7651d960dabfe9ea7895a433d756bdee1e9798
TuAnhTo <46498571+TuAnhTo@users.noreply.github.com>
2019-02-28 23:44:00 +0700

    Add files via upload

--- QuadraticEquation/PTBH.java
+++ QuadraticEquation/PTBH.java
@@ -11,8 +11,9 @@ public class PTBH {
         double b = scanner.nextDouble();
         System.out.println("Enter c  = ");
         double c = scanner.nextDouble();
-        QuadraticEquation qd = new QuadraticEquation(a , b, c);
+        QuadraticEquation KetQua = new QuadraticEquation(a , b, c);
         System.out.println(a+"x^2" + " + " + b+"x" + " + " +"c");
-        System.out.println(" Discriminant = " + qd.getDiscriminant());
+        System.out.println(" Discriminant = " + KetQua.getDiscriminant());
+        System.out.println(" nghiem cua phuong trinh: " + KetQua.getRoot1() + " nghiem 2: " + KetQua.getRoot2());
     }
 }
